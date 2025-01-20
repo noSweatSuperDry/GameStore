eff97b2c8c7ffc57978ea4e68df190070e023345
Zahid Abdullah <zaevanphy@gmail.com>
2025-01-20 14:10:23 +0200

    Async migrational

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
 
 public static class DataExtensions
 {
-public static void MigrateDb(this WebApplication app)
+public static async Task MigrateDbAsync(this WebApplication app)
 {
     using var scope = app.Services.CreateScope();
     var DbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
-    DbContext.Database.Migrate();  
+    await DbContext.Database.MigrateAsync();  
 }
 }
